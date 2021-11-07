14d0d99fbcba7ef775926a9430c9c86d3f251923
wulan17 <galihgustip@gmail.com>
2021-11-07 10:49:08 +0000

    dandelion: build Fastbootd
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- omni_dandelion.mk
+++ omni_dandelion.mk
@@ -28,6 +28,11 @@ $(call inherit-product, vendor/pb/config/common.mk)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root) \
     $(DEVICE_PATH)/prebuilt/dtb:dtb.img
 
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dandelion
 PRODUCT_NAME := omni_dandelion
